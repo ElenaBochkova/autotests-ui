68728a83907629a25e34c3e18020bf3cd4e6619a
ElenaBochkova <planeado@gmail.com>
2026-08-11 16:38:13 +0300

    Введение в Playwright. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -28,6 +28,3 @@ with sync_playwright() as playwright:
     dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
     expect(dashboard_header).to_be_visible()
     expect(dashboard_header).to_have_text("Dashboard")
-
-    # Задержка для наглядности выполнения теста (не рекомендуется использовать в реальных автотестах)
-    page.wait_for_timeout(5000)
