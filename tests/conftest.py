c6166d15e1abe2c7be51cd065ae5da253428a157
ElenaBochkova <planeado@gmail.com>
2026-08-19 17:30:14 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import pytest  # Импортируем pytest
+from playwright.sync_api import Playwright, Page
+
+@pytest.fixture(scope='session')  # Объявляем фикстуру
+def initialize_browser_state(playwright: Playwright):
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()  # Создание контекста
+    page = context.new_page()
+
+    # Переходим на страницу регистрации
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем поле email
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле username
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+
+    # Нажимаем на кнопку Registration
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    #Сохраняем контекст
+    context.storage_state(path="browser-state.json")
+
+    #Закрываем браузер
+    browser.close()
+
+
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    # Открываем браузер и создаем новую страницу с заранее созданным контекстом
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
+    yield context.new_page()
+    browser.close()
+
+                  
