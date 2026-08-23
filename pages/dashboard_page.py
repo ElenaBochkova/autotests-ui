08eb518d52c03fa5d189480372a602b6f341096e
ElenaBochkova <planeado@gmail.com>
2026-08-23 18:05:23 +0300

    Знакомство с PageObject. Практика работы с Page Object

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -7,9 +7,9 @@ class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.dasboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
 
     def check_dashboard_title(self):
-        expect(self.dasboard_title).to_be_visible()
-        expect(self.dasboard_title).to_have_text('Dashboard')
+        expect(self.dashboard_title).to_be_visible()
+        expect(self.dashboard_title).to_have_text('Dashboard')
