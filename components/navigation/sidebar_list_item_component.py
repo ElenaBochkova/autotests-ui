4af131078781db67785e28d406514d0f3789c565
ElenaBochkova <planeado@gmail.com>
2026-09-09 20:17:06 +0300

    Добавление Allure шагов в автотесты

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import Page
 from typing import Pattern
+import allure
 
 from components.base_component import BaseComponent
 from elements.icon import Icon
@@ -16,6 +17,7 @@ class SidebarListItemComponent(BaseComponent):
         self.title = Text(page, f'{identifier}-drawer-list-item-title-text', 'Sidebar item title')
         self.button = Button(page, f'{identifier}-drawer-list-item-button', 'Sidebar item button')
 
+    @allure.step('Check visible "{title}" sidebar list item') 
     def check_visible(self, title: str):
         self.icon.check_visible()
 
